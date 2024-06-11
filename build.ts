2ae3042c3a39a2ac7d2e05514aa40f250d7c372b
suhankins <punishedbernadetta@niuitmo.ru>
2024-06-11 15:10:21 +0400

    fixed build system not extracting firefox properties

--- build.ts
+++ build.ts
@@ -17,11 +17,20 @@ const processedManifest =
               ...unprocessedManifest,
               ...unprocessedManifest.chrome,
               chrome: undefined,
+              firefox: undefined,
           }
-        : { ...unprocessedManifest, chrome: undefined };
+        : {
+              ...unprocessedManifest,
+              ...unprocessedManifest.firefox,
+              firefox: undefined,
+              chrome: undefined,
+          };
 
 const directoryName = `./builds/${browser}_${formattedDate()}`;
 
 await Deno.mkdir(directoryName, { recursive: true });
-await Deno.writeTextFile(`${directoryName}/manifest.json`, JSON.stringify(processedManifest));
+await Deno.writeTextFile(
+    `${directoryName}/manifest.json`,
+    JSON.stringify(processedManifest)
+);
 await Deno.copyFile('./src/worker.js', `${directoryName}/worker.js`);
